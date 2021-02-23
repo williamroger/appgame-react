6736f13bd80b426bdb7c0ee03ce9c61d6f567b36
William Roger <williamroger.frontend@gmail.com>
2021-02-23 01:47:06 -0300

    Add lib React Hook Form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import { FiX, FiCheck } from 'react-icons/fi';
 
 import { Container, FormBody, FormFooter } from './styles';
@@ -9,21 +10,32 @@ interface FormProps {
   onClose: () => void;
 }
 
+type Input = {
+  name: string;
+  phone: string;
+  email: string;
+}
+
 const Form: React.FC<FormProps> = ({ onClose, ...rest }) => {
+  const { register, handleSubmit, watch, errors } = useForm<Input>();
+  const onSubmit = (data: any) => console.log(data);
+
+  console.log(watch('email'));
+
   return (
-    <Container>
+    <Container onSubmit={handleSubmit(onSubmit)}>
       <FormBody>
         <span>
           <label htmlFor="name">Nome</label>
-          <input id="name" name="name" placeholder="Nome do Participante" />
+          <input id="name" name="name" ref={register} placeholder="Nome do Participante" />
         </span>
         <span>
           <label htmlFor="phone-number">Celular</label>
-          <input id="phone-number" name="phone-number" placeholder="(xx) xxxxx-xxxx" />
+          <input id="phone-number" name="phone-number" ref={register} placeholder="(xx) xxxxx-xxxx" />
         </span>
         <span>
           <label htmlFor="email">E-mail</label>
-          <input id="email" name="email" placeholder="participante@email.com" />
+          <input id="email" name="email" ref={register} placeholder="participante@email.com" />
         </span>
       </FormBody>
       <FormFooter>
